19f354d59816d13a6fc537243055ea94f38948c4
jamie <jamie>
2004-12-14 01:30:45 +0000

    fix typo in credits.

--- credits.c
+++ credits.c
@@ -2,12 +2,12 @@
 
 void credits(void)
 {
-	fprintf(stderr, "\
+	fprintf(stderr, "\n\
 Released under the Gnu GPL v2\n\
 Original mfs_* package by tridge at samba.org, January 2001\n\
 Modifications by:\n\
 	adh, jamie, jbuehl, jdiner, jonbig, mbm, musclenerd, ppchacker\n\
 	and possibly others...\n\
-	A support thread is in the forums at dealdatabase.org\n\
+	A support thread is in the forums at dealdatabase.com\n\
 ");
 }
